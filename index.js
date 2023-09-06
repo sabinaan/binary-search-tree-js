0b4474ebbcf1b51ba549e211c2fe73d5b3de1adc
Sabina Andersson <sabinaan88@gmail.com>
2023-09-06 11:52:26 +0200

    Add driver script
    driver() function shows some functionalities of the program. getRandomInt() and getRandomArray() to generate a array with random numbers. checkBalanced() anc printDifferentOrders() is to make the log look better and to avoid code repetition.

--- index.js
+++ index.js
@@ -2,69 +2,75 @@ let Tree = require("./Tree.js")
 let prettyPrint = require("./prettyPrint.js")
 
 
-let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-let newTree = new Tree(testArray)
-prettyPrint(newTree.root)
+let getRandomInt = function(max = 100){
+    return Math.floor(Math.random() * max);
+}
 
-//insert
-console.log("insert 2")
-newTree.insert(2)
-prettyPrint(newTree.root)
-//newTree.insert(232)
-//prettyPrint(newTree.root)
-//newTree.insert(6700)
-//prettyPrint(newTree.root)
+let getRandomArray = function(length = 15, max = 100){
+    let array = []
+    for(i = 0 ; i < length ; i++){
+        array.push(getRandomInt(max))
+    }
+    return array
 
-//find
-//console.log(newTree.find(23))
-//console.log(newTree.find(4))
+}
 
-//delete
-console.log("delete 1")
-newTree.delete(1)
-prettyPrint(newTree.root)
-//newTree.delete(4)
-//prettyPrint(newTree.root)
+let checkBalanced = function(tree){
+    if(tree.isBalanced()){
+        console.log("The tree is balanced")
+    }else{
+        console.log("The tree is unbalanced")
+    }
+}
 
-
-
-
-let traversalTree  = new Tree([1,2,3,4,5,6,7])
-prettyPrint(traversalTree.root)
-
-//level order
-console.log("level order")
-//traversalTree.levelOrder((element) => console.log(element.value))
-console.log(traversalTree.levelOrder())
-
-//inorder
-console.log("inorder")
-console.log(traversalTree.inorder())
-//traversalTree.inorder((element) => console.log(element.value))
-
-//preorder
-console.log("preorder")
-console.log(traversalTree.preorder())
-//traversalTree.preorder((element) => console.log(element.value))
+let printDifferentOrders = function(tree){
+        //level order
+        console.log("level order")
+        tree.levelOrder((element) => console.log(element.value))
+    
+        //inorder
+        console.log("inorder")
+        console.log(tree.inorder())
+    
+        //preorder
+        console.log("preorder")
+        console.log(tree.preorder())
+    
+        //postorder
+        console.log("postorder")
+        console.log(tree.postorder())
+}
 
 
-//postorder
-console.log("postorder")
-console.log(traversalTree.postorder())
-//traversalTree.postorder((element) => console.log(element.value))
 
-console.log("height of tree: " + newTree.height(newTree.root))
-newTree.insert(8000)
-console.log("height of tree: " + newTree.height(newTree.root))
+let driver = function(){
+    let testArray = getRandomArray()
+    let testTree = new Tree(testArray)
 
-console.log(newTree.depth(newTree.find(7)))
-console.log(newTree.depth(newTree.find(9)))
+    prettyPrint(testTree.root)
 
-prettyPrint(newTree.root)
+    checkBalanced(testTree)
+    
+    printDifferentOrders(testTree)
 
-console.log(newTree.isBalanced())
-console.log(traversalTree.isBalanced())
+    //insert random numbers
+    for (i = 0 ; i < 6; i++){
+        let randomNum = getRandomInt(100)
+        testTree.insert(randomNum)
+    }
+    
+    prettyPrint(testTree.root)
+    if (!testTree.isBalanced()){
 
-newTree.rebalance()
-prettyPrint(newTree.root)
+        testTree.rebalance()
+    }
+    checkBalanced(testTree)
+
+    printDifferentOrders(testTree)
+
+
+}
+
+
+driver()
 
